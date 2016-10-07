b95292f7ef57ffdfc9a88e54c5980253b3f7534c
Thao Tran <thaotp1@fsoft.com.vn>
2016-10-07 13:46:51 +0700

    Rename

--- src/com/arg/mlt/MainRun.java
+++ src/com/arg/mlt/MainRun.java
@@ -3,12 +3,12 @@ package com.arg.mlt;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Running {
+public class MainRun {
 
 	public static void main(String[] agrs) 
 	{
 		RandomNumber random = new RandomNumber();
-		ArrayList<Integer> lst = random.createRandom( 10000, 20);
+		ArrayList<Integer> lst = random.createRandom(10000, 20);
 		Task task = new Task();
 		List<Integer> result = task.findNumDivideByThree(lst, 5, 4);
 		
